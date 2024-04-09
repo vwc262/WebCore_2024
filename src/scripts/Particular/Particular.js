debda5f657676b84c05041a624a28015df5b0382
UlisesDGM <ulises.delgadillog@gmail.com>
2024-04-09 16:57:50 -0600

    particular.js clean

--- src/scripts/Particular/Particular.js
+++ src/scripts/Particular/Particular.js
@@ -1,38 +1,3 @@
-import Estacion from "../Entities/Estacion.js";
-
-class Particular {
-  /**
-   *
-   * @param {Estacion} estacion
-   */
-
-  constructor(estacion) {
-    this.estacion = estacion;
-  }
-
-  clickEventTable() {
-    const $Tabla = document.querySelector(".curved-tBody");
-
-    $Tabla.addEventListener("click", (ev) => {
-      if (ev.target.nodeName == "DIV") {
-        [...ev.currentTarget.children].forEach((element) => {
-          element.classList.remove("tabla__active");
-        });
-
-        const actualTarget = ev.target;
-        actualTarget.classList.add("tabla__active");
-      }
-    });
-  }
-
-  loadInfo(actualTarger) {
-    // Elementos del DOM
-    const $title = document.getElementById("title");
-    const $date = document.getElementById("date");
-    const $state = document.getElementById("state");
-    
-    
-  }
-}
+class Particular {}
 
 export { Particular };
