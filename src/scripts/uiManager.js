74c669e979459875cf5999b52c006843d9d71e3d
UlisesDGM <ulises.delgadillog@gmail.com>
2024-04-09 12:02:34 -0600

    UI Manager maquetado index.html

---
+++ src/scripts/uiManager.js
@@ -0,0 +1,43 @@
+const $btnHeader = document.querySelector(".buttons__header");
+
+$btnHeader.addEventListener("click", (ev) => {
+  if (ev.target.nodeName == "DIV") {
+    [...ev.currentTarget.children].forEach((element) => {
+      element.classList.remove("header__active");
+    });
+
+    const actualTarger = ev.target;
+    actualTarger.classList.add("header__active");
+
+    switch (actualTarger.className) {
+      case "section__container header__active":
+        section__section.style.zIndex = "10";
+        section__vistaMapa.style.zIndex = "5";
+        section__graficador.style.zIndex = "5";
+        section__login.style.zIndex = "5";
+        section__particular.style.zIndex = "5";
+        break;
+      case "vistaMapa__container header__active":
+        section__section.style.zIndex = "5";
+        section__vistaMapa.style.zIndex = "10";
+        section__graficador.style.zIndex = "5";
+        section__login.style.zIndex = "5";
+        section__particular.style.zIndex = "5";
+        break;
+      case "graficador__container header__active":
+        section__section.style.zIndex = "5";
+        section__vistaMapa.style.zIndex = "5";
+        section__graficador.style.zIndex = "10";
+        section__login.style.zIndex = "5";
+        section__particular.style.zIndex = "5";
+        break;
+      case "login__container header__active":
+        section__section.style.zIndex = "5";
+        section__vistaMapa.style.zIndex = "5";
+        section__graficador.style.zIndex = "5";
+        section__login.style.zIndex = "10";
+        section__particular.style.zIndex = "5";
+        break;
+    }
+  }
+});
