4dc55ae146953061ba4a467819afd0995d7db632
UlisesDGM <ulises.delgadillog@gmail.com>
2024-04-09 12:10:16 -0600

    fix index.html

--- src/scripts/uiManager.js
+++ src/scripts/uiManager.js
@@ -10,29 +10,30 @@ $btnHeader.addEventListener("click", (ev) => {
     actualTarger.classList.add("header__active");
 
     switch (actualTarger.className) {
-      case "section__container header__active":
-        section__section.style.zIndex = "10";
+      case "home__container header__active":
+        section__home.style.zIndex = "10";
         section__vistaMapa.style.zIndex = "5";
         section__graficador.style.zIndex = "5";
         section__login.style.zIndex = "5";
         section__particular.style.zIndex = "5";
+        console.log("hola")
         break;
       case "vistaMapa__container header__active":
-        section__section.style.zIndex = "5";
+        section__home.style.zIndex = "5";
         section__vistaMapa.style.zIndex = "10";
         section__graficador.style.zIndex = "5";
         section__login.style.zIndex = "5";
         section__particular.style.zIndex = "5";
         break;
       case "graficador__container header__active":
-        section__section.style.zIndex = "5";
+        section__home.style.zIndex = "5";
         section__vistaMapa.style.zIndex = "5";
         section__graficador.style.zIndex = "10";
         section__login.style.zIndex = "5";
         section__particular.style.zIndex = "5";
         break;
       case "login__container header__active":
-        section__section.style.zIndex = "5";
+        section__home.style.zIndex = "5";
         section__vistaMapa.style.zIndex = "5";
         section__graficador.style.zIndex = "5";
         section__login.style.zIndex = "10";
