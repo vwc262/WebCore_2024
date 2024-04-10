240676df84f112faf9f816b9573e423807dc38a6
JaxterZepeda <javierzepeda1796@gmail.com>
2024-04-10 11:29:10 -0600

    Etiqueta perfil

--- src/scripts/Perfil/SitioPerfil.js
+++ src/scripts/Perfil/SitioPerfil.js
@@ -2,7 +2,7 @@ import { Core } from "../Core.js";
 import Estacion from "../Entities/Estacion.js";
 import configuracion from "../../config/PadiernaConfig.js";
 import { EventoCustomizado, EventsManager } from "../Managers/EventsManager.js";
-import { EnumTipoSignal } from "../Utilities/Enums.js";
+import { EnumUnidadesSignal } from "../Utilities/Enums.js";
 
 class SitioPerfil {
     /**
@@ -14,43 +14,39 @@ class SitioPerfil {
     }
     createSitio() {
         const estacion = Core.Instance.GetDatosEstacion(this.IdEstacion);
-        //console.log(estacion);
+        const signal = estacion.ObtenerPrimerSignal();
+        console.log(estacion);
 
         let estacionDiv = document.createElement("div");
         let nombreEstacion = document.createElement("p");
-        let nombreSignal = document.createElement("p");
-        let valorSignal = document.createElement("p");
+        let signalEtiqueta = document.createElement("p");
+
         let imagenEstacionFondoPerfil = document.createElement("img");
         let imagenEstacionNivelPerfil = document.createElement("img");
         let imagenEstacionBombaPerfil = document.createElement("img");
 
         nombreEstacion.innerText = estacion.Nombre;
-        if (estacion.Signals.length > 0 && estacion.Signals[0].Nombre.includes("Nivel")) {
-            nombreSignal.innerText = estacion.Signals[0].Nombre;
-            valorSignal.innerText = "Valor: " + estacion.Signals[0].Valor + " " +  EnumTipoSignal.Nivel;
-        }
-        else if (estacion.Signals.length > 0 && estacion.Signals[0].Nombre.includes("Presion")){
-            nombreSignal.innerText = estacion.Signals[0].Nombre;
-            valorSignal.innerText = "Valor: " + estacion.Signals[0].Valor + " " +  EnumTipoSignal.Presion;
-        }
-        else if (estacion.Signals.length > 0 && estacion.Signals[0].Nombre.includes("Gasto")){
-            nombreSignal.innerText = estacion.Signals[0].Nombre;
-            valorSignal.innerText = "Valor: " + estacion.Signals[0].Valor + " " + EnumTipoSignal.Gasto;
+
+        if (signal) {
+            signalEtiqueta.innerText = `${signal.Nombre}: ${signal.Valor} ${EnumUnidadesSignal[signal.TipoSignal]}`;
         }
 
         estacionDiv.setAttribute("class", `sitioPerfil_${estacion.Nombre}`);
-        valorSignal.setAttribute("id", estacion.IdEstacion);
+        estacionDiv.setAttribute("class", `sitioPerfil`);
+
+        nombreEstacion.setAttribute("class", `estiloNombreEstacion`); 
+        nombreEstacion.setAttribute("id", `idEstacion_${estacion.IdEstacion}`);
+        nombreEstacion.style.background = estacion.Enlace == 0 ? "red" : "green";
 
         estacionDiv.append(nombreEstacion);
-        estacionDiv.append(nombreSignal);
-        estacionDiv.append(valorSignal);
+        estacionDiv.append(signalEtiqueta);
 
-        this.ponerPosiciones(estacionDiv);
+        this.ponerEstilos(estacionDiv);
         this.suscribirEventos();
         return estacionDiv;
     }
 
-    ponerPosiciones(estacionDiv) {
+    ponerEstilos(estacionDiv) {
         const estilosEstacionEtiqueta = configuracion.perfil.estilosEstacion.find(element => element.IdEstacion == this.IdEstacion);
         if (estilosEstacionEtiqueta != undefined) {
             estacionDiv.style = estilosEstacionEtiqueta.Etiqueta;
@@ -62,8 +58,13 @@ class SitioPerfil {
     }
 
     Update() {
-        const estacion = Core.Instance.GetDatosEstacion(this.IdEstacion)
-        //console.log('Update SitioPerfil' + estacion.Nombre);
+        const estacion = Core.Instance.GetDatosEstacion(this.IdEstacion);
+        const signal = estacion.ObtenerPrimerSignal();
+
+        if (signal) {
+            const name = document.getElementById(`idEstacion_${estacion.IdEstacion}`);
+            name.style.background = estacion.Enlace == 0 ? "red" : "green";
+        }
     }
 }
 
