7346c3153b1cc09a8ad8ff298be2933608a7b3cb
Hector <vwc.hector@gmail.com>
2024-04-12 13:22:42 -0600

    avances update tabla

--- src/scripts/Tabla/Row.js
+++ src/scripts/Tabla/Row.js
@@ -14,22 +14,14 @@ class Row {
   }
 
   create() {
-    this.rowContainer = document.createElement("div");
-    this.rowContainer.classList = `sitio-tabla`;
+    const estacion = Core.Instance.GetDatosEstacion(this.IdEstacion);
 
+    this.rowContainer = CreateElement({ nodeElement: 'div', attributes: { class: `sitio-tabla` } });
     let bottomGlowColumn = CreateElement({ nodeElement: 'div', attributes: { class: 'bottomGlowColumn' } });
-
-    this.enlace = document.createElement("img");
-    this.enlace.classList = `enlace-tabla`;
-
-    let nombreFechaContainer = document.createElement("div");
-    nombreFechaContainer.classList = `nombre-Fecha-Container`;
-
-    this.nombre = document.createElement("div");
-    this.nombre.classList = `nombre-tabla`;
-
-    this.fecha = document.createElement("div");
-    this.fecha.classList = `fecha-tabla`;
+    this.enlace = CreateElement({ nodeElement: 'img', attributes: { class: `enlace-tabla` } });
+    let nombreFechaContainer = CreateElement({ nodeElement: 'div', attributes: { class: `nombre-Fecha-Container` } });;
+    this.nombre = CreateElement({ nodeElement: 'div', attributes: { class: `nombre-tabla` }, innerText: `${estacion.Nombre}` } );
+    this.fecha = CreateElement({ nodeElement: 'div', attributes: { class: `fecha-tabla` } });
 
     this.rowContainer.append(this.enlace, nombreFechaContainer, bottomGlowColumn);
     nombreFechaContainer.append(this.nombre, this.fecha);
@@ -42,13 +34,14 @@ class Row {
       Particular.Instance.mostrarDetalles();
     });
 
+    this.suscribirEventos();
+
     return this.rowContainer;
   }
 
   Update() {
     const estacion = Core.Instance.GetDatosEstacion(this.IdEstacion);
 
-    this.nombre.innerText = `${estacion.Nombre}`;
     this.enlace.setAttribute(
       "src",
       `${Core.Instance.ResourcesPath}General/state_${estacion.Enlace}.png?v=0`
@@ -56,9 +49,9 @@ class Row {
     this.fecha.innerText = `${estacion.ObtenerFecha()}`;
   }
 
-  // suscribirEventos() {
-  //     EventsManager.Instance.Suscribirevento('Update', new EventoCustomizado(() => this.updateEstacion()));
-  // }
+  suscribirEventos() {
+    EventsManager.Instance.Suscribirevento('Update', new EventoCustomizado(() => this.Update()));
+  }
 }
 
 export { Row };
