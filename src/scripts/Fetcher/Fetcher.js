2a40797035360fa68560f86c00d883f66d350372
UlisesDGM <ulises.delgadillog@gmail.com>
2024-05-06 15:13:08 -0600

    Tabla simplificada
    se agrego la funcionalidad de agregar notas a los sitios de padierna

--- src/scripts/Fetcher/Fetcher.js
+++ src/scripts/Fetcher/Fetcher.js
@@ -1,59 +1,59 @@
 import { RequestType } from "../Utilities/Enums.js";
 
-
 /**
  * Clase base para request
  */
 class Fetcher {
-    #root = 'http://w1.doomdns.com:11000/vwctestapi/crud';
-    #rootVersion = 'http://w1.doomdns.com:11000/vwctestapi/proyecto';
-    static #_instance = undefined;
-    /**
-     * @returns {Fetcher}
-     */
-    static get Instance() {
-        if (!this.#_instance) {
-            this.#_instance = new Fetcher();
-        }
-        return this.#_instance;
+  isLogged = false;
+  #root = "http://w1.doomdns.com:11000/vwctestapi/crud";
+  #rootVersion = "http://w1.doomdns.com:11000/vwctestapi/proyecto";
+  static #_instance = undefined;
+  /**
+   * @returns {Fetcher}
+   */
+  static get Instance() {
+    if (!this.#_instance) {
+      this.#_instance = new Fetcher();
     }
-    /**
-     * Request de informacion a la API
-     * @param {string} action 
-     * @param {RequestType } requestType 
-     * @param {boolean} doSerialize 
-     * @returns {object}
-     */
-    async RequestData(action, requestType, data, doSerialize) {
-        const config = {
-            method: requestType,
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: doSerialize ? JSON.stringify(data) : data
-        };
-        if (requestType == RequestType.GET) {
-            delete config.body;
-            delete config.headers;
-        }
-        const response = await fetch(`${this.#root}/${action}`, config);
-        return await response.json();
+    return this.#_instance;
+  }
+  /**
+   * Request de informacion a la API
+   * @param {string} action
+   * @param {RequestType } requestType
+   * @param {boolean} doSerialize
+   * @returns {object}
+   */
+  async RequestData(action, requestType, data, doSerialize) {
+    const config = {
+      method: requestType,
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: doSerialize ? JSON.stringify(data) : data,
+    };
+    if (requestType == RequestType.GET) {
+      delete config.body;
+      delete config.headers;
     }
+    const response = await fetch(`${this.#root}/${action}`, config);
+    return await response.json();
+  }
 
-    /**
-     * 
-     * @param {string} action 
-     * @returns {number} number
-     */
-    async RequestVersion(action){
-        const config = {
-            method: 'get',
-            mode: 'cors',
-        };
-        const response = await fetch(`${this.#rootVersion}/${action}`, config);
-        return await response.json();
-    }
+  /**
+   *
+   * @param {string} action
+   * @returns {number} number
+   */
+  async RequestVersion(action) {
+    const config = {
+      method: "get",
+      mode: "cors",
+    };
+    const response = await fetch(`${this.#rootVersion}/${action}`, config);
+    return await response.json();
+  }
 }
 
-export { Fetcher }
+export { Fetcher };
