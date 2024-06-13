7fddb0fb5ba0ae573c4ee6b73a8a336486db408b
JaxterZepeda <javierzepeda1796@gmail.com>
2024-06-13 15:44:26 -0600

    arreglo de medidas

--- src/config/ChiconautlaConfig.js
+++ src/config/ChiconautlaConfig.js
@@ -7,84 +7,84 @@ export const ChiconautlaConfig = {
         estilosEstacion: [
             { 
                 "IdEstacion": 1, // VO-01
-                "Imagen": "position:absolute; width:33px;height:79px; left:732px; top:565px;", 
+                "Imagen": "position:absolute; width:37px;height:75px; left:732px; top:565px;", 
                 "Etiqueta": "position:absolute;width:140px !important; height:auto;left:908px;top:637px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(-3deg) rotateZ(2deg)" 
             },
 
             { 
                 "IdEstacion": 3, // VO-03
-                "Imagen": "position:absolute;width:33px;height:79px;left: 225px;top: 870px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left: 222px;top: 869px;", 
                 "Etiqueta": "position:absolute;width:140px !important; height:auto;left:100px;top:821px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(2000px) rotateY(11deg) rotateX(-15deg) rotateZ(-2deg)" 
             },
 
             { 
                 "IdEstacion": 4, // TN-02
-                "Imagen": "position:absolute;width:33px;height:79px;left:678px;top:593px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:678px;top:593px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:832px;top:679px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(5000px) rotateY(23deg) rotateX(-13deg) rotateZ(-7deg)" 
             },
 
             { 
                 "IdEstacion": 6, // VO-06
-                "Imagen": "position:absolute;width:33px;height:79px;left:874px;top:488px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:874px;top:488px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:981px;top:590px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 7, // VO-07
-                "Imagen": "position:absolute;width:33px;height:79px;left:920px;top:463px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:920px;top:463px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:1068px;top:549px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(3000px) rotateY(-20deg) rotateX(-19deg) rotateZ(9deg)" 
             },
 
             { 
                 "IdEstacion": 8, // VO-08
-                "Imagen": "position:absolute;width:33px;height:79px;left:965px;top:441px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:965px;top:441px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:1137px;top:507px;",
                 "Estado": "width:384px;height:175px;transform:perspective(4002px) rotateY(-6deg) rotateX(-20deg) rotateZ(3deg)" 
             },
 
             { 
                 "IdEstacion": 9, // VO-09
-                "Imagen": "position:absolute;width:33px;height:79px;left:1010px;top:416px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:1010px;top:416px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:1225px;top:465px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(10deg) rotateX(-17deg) rotateZ(-2deg)"
             },
 
             { 
                 "IdEstacion": 10, // VO-10
-                "Imagen": "position:absolute;width:33px;height:79px;left:1060px;top:392px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:1060px;top:392px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:976px;top:338px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(29deg) rotateX(-8deg) rotateZ(-5deg)"
             },
 
             { 
                 "IdEstacion": 11, // VO-11
-                "Imagen": "position:absolute;width:33px;height:79px;left:1117px;top:370px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:1117px;top:370px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:1030px;top:292px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(2000px) rotateY(0deg) rotateX(-20deg) rotateZ(-1deg)" 
             },
 
             { 
                 "IdEstacion": 12, // VO-12
-                "Imagen": "position:absolute;width:33px;height:79px;left:1177px;top:352px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:1177px;top:352px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:1104px;top:250px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 13, // VO-13
-                "Imagen": "position:absolute;width:33px;height:79px;left:1240px;top:341px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:1240px;top:341px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:1175px;top:209px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(5000px) rotateY(-16deg) rotateX(-21deg) rotateZ(7deg)" 
             },
 
             { 
                 "IdEstacion": 14, // VO-14
-                "Imagen": "position:absolute;width:33px;height:79px;left:1317px;top:331px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:1317px;top:331px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:1263px;top:163px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(8deg) rotateX(0deg) rotateZ(0deg)" 
             },
@@ -98,161 +98,161 @@ export const ChiconautlaConfig = {
 
             { 
                 "IdEstacion": 16, // VENTA DE CARPIO
-                "Imagen": "position:absolute;width:33px;height:79px;left:0px;top:0px;display:none", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:0px;top:0px;display:none", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:0px;top:0px;display:none", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 17, // TN-03
-                "Imagen": "position:absolute;width:33px;height:79px;left:1065px;top:877px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:1065px;top:877px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:1125px;top:827px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(23deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 18, // VC-01
-                "Imagen": "position:absolute; width:33px; height:79px; left:465px; top:643px;", 
+                "Imagen": "position:absolute; width:37px; height:75px; left:465px; top:643px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:278px;top:689px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(3000px) rotateY(-17deg) rotateX(-21deg) rotateZ(9deg)" 
             },
 
             { 
                 "IdEstacion": 19, //VCH-01
-                "Imagen": "position:absolute;width:33px;height:79px;left:587px;top:691px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:587px;top:691px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:684px;top:750px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(3000px) rotateY(-17deg) rotateX(-21deg) rotateZ(9deg)" 
             },
 
             { 
                 "IdEstacion": 20, //VCH-03
-                "Imagen": "position:absolute;width:33px;height:79px;left:571px;top:772px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:571px;top:772px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:665px;top:819px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(-40deg) rotateX(-17deg) rotateZ(15deg)" 
             },
 
             { 
                 "IdEstacion": 21, // CH-02
-                "Imagen": "position:absolute;width:33px;height:79px;left:695px;top:306px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:695px;top:306px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:610px;top:224px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(6deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 22, // CH-04
-                "Imagen": "position:absolute;width:33px;height:79px;left:738px;top:289px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:738px;top:289px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:647px;top:182px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 23, // falta etiqueta GC-01
-                "Imagen": "position:absolute;width:33px;height:79px;left:386px;top:425px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:386px;top:425px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:478px;top:496px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(2000px) rotateY(22deg) rotateX(-12deg) rotateZ(-5deg)" 
             },
 
             { 
                 "IdEstacion": 24, // GC-02
-                "Imagen": "position:absolute;width:33px;height:79px;left:452px;top:400px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:452px;top:400px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:358px;top:252px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(13deg) rotateX(-8deg) rotateZ(-2deg)" 
             },
 
             { 
                 "IdEstacion": 25, // GC-03
-                "Imagen": "position:absolute;width:33px;height:79px;left:509px;top:378px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:509px;top:378px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:447px;top:199px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 26, // GC-04
-                "Imagen": "position:absolute;width:33px;height:79px;left:780px;top:272px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:780px;top:272px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:688px;top:140px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(-5deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 27, // SC-03
-                "Imagen": "position:absolute;width:33px;height:79px;left:331px;top:330px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:331px;top:330px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:177px;top:295px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(-6deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 28, //SC-04
-                "Imagen": "position:absolute;width:33px;height:79px;left:254px;top:359px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:254px;top:359px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:98px;top:358px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(-13deg) rotateX(-14deg) rotateZ(3deg)" 
             },
 
             { 
                 "IdEstacion": 29, //TN-04
-                "Imagen": "position:absolute;width:33px;height:79px;left:651px;top:325px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:651px;top:325px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:552px;top:267px;", 
                 "Estado": "width:384px;height:175px;transform:perspective(2000px) rotateY(-50deg) rotateX(-12deg) rotateZ(15deg)" 
             },
 
             { 
                 "IdEstacion": 30, // KM25
-                "Imagen": "position:absolute;width:33px;height:79px;left: 584px;top:375px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left: 584px;top:375px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:509px;top:375px;display:none", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 31, //TC-1
-                "Imagen": "position:absolute;width:33px;height:79px;left:903px;top:225px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:903px;top:225px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:965px;top:158px", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 32, //TC-2
-                "Imagen": "position:absolute;width:33px;height:79px;left:860px;top:242px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:860px;top:242px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:864px;top:102px", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 33, // TC-3
-                "Imagen": "position:absolute;width:33px;height:79px;left:226px;top:493px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:226px;top:493px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:84px;top:458px", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 34, //SC-1 FALTA ETIQUETA
-                "Imagen": "position:absolute;width:33px;height:79px;left:322px;top:450px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:322px;top:450px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:407px;top:537px", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 35, //SC-2
-                "Imagen": "position:absolute;width:33px;height:79px;left:820px;top:257px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:820px;top:257px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:725px;top:98px", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 36, //VO-4
-                "Imagen": "position:absolute;width:33px;height:79px;left:929px;top:881px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:929px;top:881px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:984px;top:827px", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 37, // VCH-4
-                "Imagen": "position:absolute;width:33px;height:79px;left:555px;top:851px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:555px;top:851px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:637px;top:885px", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
 
             { 
                 "IdEstacion": 38, // TN-1
-                "Imagen": "position:absolute;width:33px;height:79px;left:806px;top:882px;", 
+                "Imagen": "position:absolute;width:37px;height:75px;left:806px;top:882px;", 
                 "Etiqueta": "position:absolute;width:140px !important ;height:auto;left:841px;top:827px", 
                 "Estado": "width:384px;height:175px;transform:perspective(1000px) rotateY(0deg) rotateX(0deg) rotateZ(0deg)" 
             },
@@ -293,7 +293,7 @@ export const ChiconautlaConfig = {
                 { 
                     "idLinea": 6, 
                     "idEstacion": 0, 
-                    "Tag": "Gravedad_6", "css": "position: absolute;top: 672px;left: 584px;transform: rotate(-164deg);width: 47px;height: 10px;border-radius: 15px;background-size: 100% 100% !important;z-index: 1;border: 1px solid #00a8b1;" 
+                    "Tag": "Gravedad_6", "css": "position: absolute;top: 673px;left: 584px;transform: rotate(14deg);width: 47px;height: 10px;border-radius: 15px;background-size: 100% 100% !important;z-index: 1;border: 1px solid #00a8b1;" 
                 },
 
                 { 
