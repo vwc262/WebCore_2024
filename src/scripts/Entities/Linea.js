00e8cd097d6b3ade66d414e2e7a26f548ad0b091
Hector <vwc.hector@gmail.com>
2025-03-04 13:30:58 -0600

    init

--- src/scripts/Entities/Linea.js
+++
@@ -1,9 +0,0 @@
-class Linea {
-    constructor(lineaCruda) {
-        this.IdEstacion = lineaCruda.idEstacion;
-        this.IdLinea = lineaCruda.idLinea;
-        this.Nombre = lineaCruda.nombre;
-    }
-}
-
-export default Linea;
