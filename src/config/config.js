8e8cd07c7486b1e05c50282ae8092010898bdb91
Hector <vwc.hector@gmail.com>
2025-05-06 17:35:41 -0600

    avances iztapalapa

--- src/config/config.js
+++ src/config/config.js
@@ -4,12 +4,11 @@ import { PozosAIFAConfig } from "./AIFA.js";
 import { ChalmitaConfig } from "./ChalmitaConfig.js";
 import { ChiconautlaConfig, NombresLargosChiconautla } from "./ChiconautlaConfig.js";
 import { ClimatologicasHidrometricasConfig } from "./ClimatologicasHidrometricas2024Config.js";
-import { EncharcamientosConfig } from "./EncharcamientosConfig.js";
 import { GAMPonchoConfig } from "./GAMPonchoConfig.js";
 import { GustavoAMaderoConfig } from "./GustavoAMaderoConfig.js";
+import { IztapalapaConfig } from "./IztapalapaConfig.js";
 import { LineaMoradaConfig } from "./LineaMorada.js";
 import { PadiernaConfig } from "./PadiernaConfig.js";
-import { PlantasPotabilizadoras } from "./PlantasPotabilizadoras.js";
 import { PozosSistemaLermaConfig } from "./PozosSistemaLermaConfig.js";
 import { SantaCatarinaConfig } from "./SantaCatarinaConfig.js";
 import { TeoloyucanConfig } from "./Teoloyucan.js";
@@ -23,35 +22,50 @@ var Configuracion = {
     GetConfiguracion(enumProyecto) {
         let config = {};
         switch (enumProyecto) {
-            case EnumProyecto.Padierna:
-                config = PadiernaConfig;
-                break;
-            case EnumProyecto.SantaCatarina:
-                config = SantaCatarinaConfig;
-                break;
             case EnumProyecto.GustavoAMadero:
                 config = GustavoAMaderoConfig;
                 break;
-            case EnumProyecto.Chiconautla:
-                config = ChiconautlaConfig;
-                break;
-            case EnumProyecto.Encharcamientos:
-                config = EncharcamientosConfig;
+            case EnumProyecto.Padierna:
+                config = PadiernaConfig;
                 break;
             case EnumProyecto.Lerma:
                 config = PozosSistemaLermaConfig;
                 break;
-            case EnumProyecto.PlantasPotabilizadoras:
-                config = PlantasPotabilizadoras;
+            case EnumProyecto.Iztapalapa:
+                config = IztapalapaConfig;
                 break;
-            case EnumProyecto.ClimatologicasHidrometricas:
-                config = ClimatologicasHidrometricasConfig
+            case EnumProyecto.Chalmita:
+                config = ChalmitaConfig;
+                break;
+            case EnumProyecto.Yaqui:
+                config = undefined;
+                break;
+            case EnumProyecto.SistemaCutzamala:
+                config = undefined;
                 break;
             case EnumProyecto.PruebasCampo:
                 config = GAMPonchoConfig;
                 break;
+            case EnumProyecto.SantaCatarina:
+                config = SantaCatarinaConfig;
+                break;
+            case EnumProyecto.Chiconautla:
+                config = ChiconautlaConfig;
+                break;
+            case EnumProyecto.Sorpasso:
+                config = undefined;
+                break;
+            case EnumProyecto.EscudoNacional:
+                config = undefined;
+                break;
+            case EnumProyecto.ClimatologicasHidrometricas:
+                config = ClimatologicasHidrometricasConfig
+                break;
             case EnumProyecto.Teoloyucan:
                 config = TeoloyucanConfig;
+                break;
+            case EnumProyecto.Pruebas:
+                config = undefined;
                 break;
             case EnumProyecto.LineaMorada:
                 config = LineaMoradaConfig;
@@ -59,14 +73,35 @@ var Configuracion = {
             case EnumProyecto.PozosAIFA:
                 config = PozosAIFAConfig;
                 break;
-            case EnumProyecto.Chalmita:
-                config = ChalmitaConfig;
+            case EnumProyecto.PozosZumpango:
+                config = undefined;
                 break;
-            case EnumProyecto.PozosReyesFerrocarril:
+            case EnumProyecto.PaseoDelRio:
                 config = undefined;
                 break;
             case EnumProyecto.Aduana:
                 config = AduanaConfig;
+                break;
+            case EnumProyecto.PozosPAI:
+                config = undefined;
+                break;
+            case EnumProyecto.PozosCoyoacan:
+                config = undefined;
+                break;
+            case EnumProyecto.PozosAzcapotzalco:
+                config = undefined;
+                break;
+            case EnumProyecto.PozosPAI:
+                config = undefined;
+                break;
+            case EnumProyecto.Encharcamientos:
+                config = undefined
+                break;
+            case EnumProyecto.Lumbreras:
+                config = undefined;
+                break;
+            case EnumProyecto.Ramales:
+                config = undefined;
                 break;
         }
 
