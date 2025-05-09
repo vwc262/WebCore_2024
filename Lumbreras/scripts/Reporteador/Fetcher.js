41383f466db302bf613300be86081b5ceb91ab06
Hector <vwc.hector@gmail.com>
2025-05-09 09:46:43 -0600

    Lumbreras

---
+++ Lumbreras/scripts/Reporteador/Fetcher.js
@@ -0,0 +1,81 @@
+import { Core } from "../Core.js";
+import { EnumProyecto } from "../Utilities/Enums.js";
+
+const FetcherGraficador = {
+  SinData: {},
+  methodType: {
+    GET: "GET",
+    POST: "POST",
+  },
+  version: 0,
+  uriAssets: "https://virtualwavecontrol.com.mx/RecursosWeb/WebCore24/",
+  uri: "https://virtualwavecontrol.com.mx/api24/VWC/app2024/", 
+  /**
+   * Funcion para hacer request del tipo get y post , Nota : Si es tipo Post jsonizar debe estar en true
+   * @param {{action : string, method : string, data = {}, jsonizar = boolean}} ParametrosOpcionales
+   * @returns
+   */
+  request: async function ({
+    action = EnumControllerPoleo.CONSULTAR,
+    method = this.methodType.GET,
+    data = this.SinData,
+    jsonizar = true,
+    project = Core.Instance.IdProyecto,
+  }) {
+    const options = {
+      method: method,
+      mode: "cors",
+      headers: {
+        // 'Accept':'text/plain',
+        "Content-Type": "application/json",
+      },
+      body: jsonizar ? JSON.stringify(data) : data,
+    };
+
+    //Los metodos get no aceptan body
+    if (method == "GET") {
+      delete options.body;
+      delete options.headers;
+    }
+
+    const fetchresult = await fetch(`${this.uri}/${action}?idProyecto=${Core.Instance.IdProyecto}`, options);
+    let jsonData = null;
+
+    jsonData = await fetchresult.json();
+    return jsonData;
+  },
+  getImage: function (projectName, folderRoot, assetName, ext) {
+    return `${Core.Instance.ResourcesPath}${folderRoot}/${assetName}.${ext}?v=${this.version}`;
+  }
+};
+
+const EnumPeticiones = {
+  READ: "getInfraestructura",
+  HISTORICOS: "GetReportes",
+};
+
+const EnumNameProjecto = {
+  TanquesPadierna: 'TanquesPadierna',
+}
+
+const getNombreProyectoIdProyecto = (idProyecto) => {
+  switch (idProyecto) {
+    case EnumProyecto.GustavoAMadero: return "GustavoAMadero";
+    case EnumProyecto.Padierna: return "Padierna";
+    case EnumProyecto.Lerma: return "Lerma";
+    case EnumProyecto.Yaqui: return "Yaqui";
+    case EnumProyecto.Chalmita: return "Chalmita";
+    case EnumProyecto.Encharcamientos: return "Encharcamientos";
+    case EnumProyecto.Sectores: return "Sectores";
+    case EnumProyecto.PruebasCampo: return "PruebasCampo";
+    case EnumProyecto.SantaCatarina: return "SantaCatarina";
+    case EnumProyecto.Chiconautla: return "Chiconautla";
+    case EnumProyecto.Sorpasso: return "Sorpasso";
+    case EnumProyecto.SistemaCutzamala: return "SistemaCutzamala";
+    case EnumProyecto.ClimatologicasHidrometricas: return "ClimatologicasHidrometricas";
+    case EnumProyecto.Pruebas: return "Pruebas";
+    case EnumProyecto.LineaMorada: return "LineaMorada";
+  }
+
+}
+export { FetcherGraficador, EnumPeticiones, EnumProyecto, EnumNameProjecto, getNombreProyectoIdProyecto };
---
+++ Lumbreras/scripts/Tabla/Row.js
@@ -0,0 +1,112 @@
+import Estacion from "../Entities/Estacion.js";
+import { Core } from "../Core.js";
+import { EventoCustomizado, EventsManager } from "../Managers/EventsManager.js";
+import { Particular } from "../Particular/Particular.js";
+import { CreateElement } from "../Utilities/CustomFunctions.js";
+import { Tabla } from "./Tabla.js";
+import { GoHome, Module, SetActualModule } from "../uiManager.js";
+import { EnumModule, EnumTipoSignal } from "../Utilities/Enums.js";
+import { Configuracion } from "../../config/config.js";
+import { ArranqueParo } from "../ArranqueParo/ArranqueParo.js";
+
+class Row {
+  /**
+   *
+   * @param {Estacion} estacion
+   * @param {Function} hoverRow callback
+   */
+  constructor(IdEstacion, hoverRow) {
+    this.IdEstacion = IdEstacion;
+    this.hoverRow = hoverRow;
+  }
+
+  create() {
+    const estacion = Core.Instance.GetDatosEstacion(this.IdEstacion);
+
+    this.rowContainer = CreateElement({
+      nodeElement: 'div',
+      attributes: { class: `sitio-tabla`, style: 'cursor:pointer' },
+      innerText: '',
+      events: new Map()
+        .set('mouseover', [(event) => {
+          this.hoverRow(true, this.IdEstacion);
+        }])
+        .set('mouseout', [(event) => {
+          this.hoverRow(false, this.IdEstacion);
+        }])
+    });
+
+    let bottomGlowColumn = CreateElement({ nodeElement: 'div', attributes: { class: 'bottomGlowColumn' } });
+    this.enlace = CreateElement({ nodeElement: 'img', attributes: { class: `enlace-tabla` } });
+    let nombreFechaContainer = CreateElement({ nodeElement: 'div', attributes: { class: `nombre-Fecha-Container` } });;
+    this.nombre = CreateElement({ nodeElement: 'div', attributes: { class: `nombre-tabla` }, innerText: `${estacion.Nombre}` });
+    this.fecha = CreateElement({ nodeElement: 'div', attributes: { class: `fecha-tabla` } });
+
+    this.rowContainer.append(this.enlace, nombreFechaContainer, bottomGlowColumn);
+    nombreFechaContainer.append(this.nombre, this.fecha);
+
+    this.Update();
+
+    this.rowContainer.addEventListener("click", (event) => {
+
+      const estacion = Core.Instance.GetDatosEstacion(this.IdEstacion);
+      const index = Core.Instance.data.indexOf(estacion);
+
+      if (Module == EnumModule.Perfil || Module == EnumModule.Particular) {
+        this.ElegirPanelOParticular(estacion);
+      } else if (Module == EnumModule.Mapa) {
+        EventsManager.Instance.EmitirEvento('OnClickTablaToMarker', { dataMarker: estacion });
+      }
+
+      let options = document.querySelector('ul.options');
+      if (options.children.length > 0) {
+        let ul = options.children[index];
+        ul.click();
+      }
+
+    });
+
+    this.suscribirEventos();
+
+    return this.rowContainer;
+  }
+  ElegirPanelOParticular(estacion) {
+    const configProyecto = Configuracion.GetConfiguracion(Core.Instance.IdProyecto);
+    let mostrarPanel = false;
+
+    if (configProyecto.estacionesSinParticular) {
+      mostrarPanel = configProyecto.estacionesSinParticular.includes(estacion.IdEstacion);
+    }
+
+    let tieneBombas = estacion.Signals.filter(s => s.TipoSignal == EnumTipoSignal.Bomba)?.length > 0;
+
+    if (mostrarPanel) {
+      SetActualModule("Perfil");
+      GoHome();
+
+      if (tieneBombas){
+        ArranqueParo.Instance.Create(estacion.IdEstacion);
+      }
+    }
+    else {
+      Particular.Instance.setEstacion(estacion);
+      Particular.Instance.mostrarDetalles();
+    }
+  }
+
+  Update() {
+    const estacion = Core.Instance.GetDatosEstacion(this.IdEstacion);
+
+    this.enlace.setAttribute(
+      "src",
+      `${Core.Instance.ResourcesPath}General/state_${estacion.IsTimeout() ? 't' : estacion.IsEnMantenimiento() ? 'm' : estacion.Enlace}.png?v=${Core.Instance.version}`
+    );
+    this.fecha.innerText = `${estacion.ObtenerFecha()}`;
+  }
+
+  suscribirEventos() {
+    EventsManager.Instance.Suscribirevento('Update', new EventoCustomizado(() => this.Update()));
+  }
+}
+
+export { Row };
