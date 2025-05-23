e3f857cb5b79142425b6877f72a6b7612b2b6720
MIKE-RED <miguelrojasmarc99@gmail.com>
2025-05-23 15:56:02 -0600

    Pozos Azacapotzalco Config

--- Pozos2025/config/PozosAzcapotzalco.js
+++ Pozos2025/config/PozosAzcapotzalco.js
@@ -8,146 +8,140 @@ export const PozosAzcapotzalco = {
             /* pozos Azcapotzalco*/
 			{ 
                 "IdEstacion": 1, //Ahuitzotla
-                "Imagen": "position: absolute;      left: 145px; top: 650px; ",
+                "Imagen": "position: absolute;      left: 145px; top: 647px; ",
                 "Etiqueta": "position: absolute;    left: 132px; top: 606px; ",
                 "Estado": "display:none;"
             },
 			{
                 "IdEstacion": 2, //PB Atoron
-                "Imagen": "position: absolute; 		left: 1040px; top: 618px; ",
-                "Etiqueta": "position: absolute; 	left: 1027px; top: 575px; ",
+                "Imagen": "position: absolute; 		left: 1039px; top: 607px; ",
+                "Etiqueta": "position: absolute; 	left: 1027px; top: 565px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 3, //Campo Encantado
-                "Imagen": "position: absolute;      left: 312px; top: 579px; ",
-                "Etiqueta": "position: absolute;    left: 312px; top: 579px; ",
+                "Imagen": "position: absolute;      left: 340px; top: 662px; ",
+                "Etiqueta": "position: absolute;    left: 328px; top: 620px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 4, //Deportivo Reynosa
-                "Imagen": "position: absolute;      left: 862px; top: 460px; ",
-                "Etiqueta": "position: absolute;    left: 849px; top: 417px; ",
+                "Imagen": "position: absolute;      left: 672px; top: 502px; ",
+                "Etiqueta": "position: absolute;    left: 660px; top: 460px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 5, //Ferreria 2
-                "Imagen": "position: absolute; 		left: 963px; top: 606px; ",
-                "Etiqueta": "position: absolute; 	left: 950px; top: 563px; ",
+                "Imagen": "position: absolute; 		left: 897px; top: 686px; ",
+                "Etiqueta": "position: absolute; 	left: 885px; top: 645px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 6, //Potanco 1
-                "Imagen": "position: absolute; 		left: 351px; top: 316px; ",
-                "Etiqueta": "position: absolute; 	left: 338px; top: 273px; ",
+                "Imagen": "position: absolute; 		left: 1052px; top: 777px; ",
+                "Etiqueta": "position: absolute; 	left: 1040px; top: 735px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 7, //Potanco 2
-                "Imagen": "position: absolute; 		left: 792px; top: 145px; ",
-                "Etiqueta": "position: absolute; 	left: 779px; top: 102px; ",
+                "Imagen": "position: absolute; 		left: 579px; top: 472px; ",
+                "Etiqueta": "position: absolute; 	left: 567px; top: 430px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 8, //Potanco 3
-                "Imagen": "position: absolute; 		left: 1058px; top: 289px; ",
-                "Etiqueta": "position: absolute; 	left: 1045px; top: 246px; ",
+                "Imagen": "position: absolute; 		left: 752px;  top: 557px; ",
+                "Etiqueta": "position: absolute; 	left: 740px;  top: 515px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 9, //Prados del Rosario
-                "Imagen": "position: absolute; 		left: 1013px; top: 452px; ",
-                "Etiqueta": "position: absolute; 	left: 1000px; top: 409px; ",
+                "Imagen": "position: absolute; 		left: 275px; top: 542px; ",
+                "Etiqueta": "position: absolute; 	left: 262px; top: 500px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 10, //San juan Tlilhuacan
-                "Imagen": "position: absolute; 		left: 420px; top: 741px; ",
-                "Etiqueta": "position: absolute; 	left: 407px; top: 698px; ",
+                "Imagen": "position: absolute; 		left: 352px; top: 600px; ",
+                "Etiqueta": "position: absolute; 	left: 340px; top: 560px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 11, //PB Trabajadores de Hierro
-                "Imagen": "position: absolute; 		left: 137px; top: 799px; ",
-                "Etiqueta": "position: absolute; 	left: 124px; top: 756px; ",
+                "Imagen": "position: absolute; 		left: 1095px; top: 676px; ",
+                "Etiqueta": "position: absolute; 	left: 1083px; top: 634px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 12, //ProHogar
-                "Imagen": "position: absolute; 		left: 34px; top: 816px; ",
-                "Etiqueta": "position: absolute; 	left: 21px; top: 773px; ",
+                "Imagen": "position: absolute; 		left: 1000px; top: 723px; ",
+                "Etiqueta": "position: absolute; 	left: 987px;  top: 681px; ",
                 "Estado": "display:none;"
             },
             {
-                "IdEstacion": 13, //Nueva Snata Maria
-                "Imagen": "position: absolute; 		left: 746px; top: 545px; ",
-                "Etiqueta": "position: absolute; 	left: 733px; top: 502px; ",
+                "IdEstacion": 13, //Nueva Santa Maria
+                "Imagen": "position: absolute; 		left: 812px; top: 817px; ",
+                "Etiqueta": "position: absolute; 	left: 800px; top: 775px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 14, //Hogar y Seguridad
-                "Imagen": "position: absolute; 		left: 841px; top: 329px; ",
-                "Etiqueta": "position: absolute; 	left: 828px; top: 286px; ",
+                "Imagen": "position: absolute; 		left: 752px; top: 777px; ",
+                "Etiqueta": "position: absolute; 	left: 740px; top: 735px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 15, //Jardin San Alvaro
-                "Imagen": "position: absolute; 		left: 750px; top: 286px; ",
-                "Etiqueta": "position: absolute; 	left: 737px; top: 243px; ",
+                "Imagen": "position: absolute; 		left: 494px; top: 846px; ",
+                "Etiqueta": "position: absolute; 	left: 482px; top: 805px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 16, //Obrero Popular
-                "Imagen": "position: absolute; 		left: 638px; top: 411px; ",
-                "Etiqueta": "position: absolute; 	left: 625px; top: 368px; ",
+                "Imagen": "position: absolute; 		left: 683px; top: 818px; ",
+                "Etiqueta": "position: absolute; 	left: 670px; top: 776px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 17, //Claveria
-                "Imagen": "position: absolute; 		left: 719px; top: 604px; ",
-                "Etiqueta": "position: absolute; 	left: 706px; top: 561px; ",
+                "Imagen": "position: absolute; 		left: 529px; top: 811px; ",
+                "Etiqueta": "position: absolute; 	left: 516px; top: 770px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 18, //San Miguel Amantla
-                "Imagen": "position: absolute; 		left: 1109px; top: 511px; ",
-                "Etiqueta": "position: absolute; 	left: 1096px; top: 468px; ",
+                "Imagen": "position: absolute; 		left: 188px; top: 710px; ",
+                "Etiqueta": "position: absolute; 	left: 176px; top: 669px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 19, // Popotla
-                "Imagen": "position: absolute; 		left: 710px; top: 464px; ",
-                "Etiqueta": "position: absolute; 	left: 697px; top: 421px; ",
+                "Imagen": "position: absolute; 		left: 503px; top: 454px; ",
+                "Etiqueta": "position: absolute; 	left: 491px; top: 413px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 20, //PB Panamericana
-                "Imagen": "position: absolute; 		left: 973px; top: 326px; ",
-                "Etiqueta": "position: absolute; 	left: 962px; top: 283px; ",
+                "Imagen": "position: absolute; 		left: 1195px; top: 684px; ",
+                "Etiqueta": "position: absolute; 	left: 1183px; top: 643px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 21, //Marina Nacional 3
-                "Imagen": "position: absolute; 		left: 991px; top: 236px; ",
-                "Etiqueta": "position: absolute; 	left: 978px; top: 193px; ",
+                "Imagen": "position: absolute; 		left: 373px; top: 986px; ",
+                "Etiqueta": "position: absolute; 	left: 361px; top: 944px; ",
                 "Estado": "display:none;"
             },
             {
                 "IdEstacion": 22, //Marina Nacional 4
-                "Imagen": "position: absolute; 		left: 1262px; top: 395px; ",
-                "Etiqueta": "position: absolute; 	left: 1249px; top: 352px; ",
+                "Imagen": "position: absolute; 		left: 324px; top: 921px; ",
+                "Etiqueta": "position: absolute; 	left: 312px; top: 879px; ",
                 "Estado": "display:none;"
             },
             {
-                "IdEstacion": 23, //san luis 1 
-                "Imagen": "position: absolute; 		left: 735px; top: 398px; ",
-                "Etiqueta": "position: absolute; 	left: 722px; top: 355px; ",
-                "Estado": "display:none;"
-            },
-            {
-                "IdEstacion": 24, //Mar Mediterraneo
-                "Imagen": "position: absolute; 		left: 1239px; top: 595px; ",
-                "Etiqueta": "position: absolute; 	left: 1226px; top: 552px; ",
+                "IdEstacion": 23, //Mar Mediterraneo
+                "Imagen": "position: absolute; 		left: 617px; top: 990px; ",
+                "Etiqueta": "position: absolute; 	left: 605px; top: 948px; ",
                 "Estado": "display:none;"
             }
             
