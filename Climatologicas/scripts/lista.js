27fb2173ba0b177149b06a02e12aae3eac88ef0c
UlisesDGM <ulises.delgadillog@gmail.com>
2025-05-28 16:20:59 -0600

    avance maqueta

--- Climatologicas/scripts/lista.js
+++ Climatologicas/scripts/lista.js
@@ -1,5 +1,38 @@
 export function initLista() {
   animar_lista();
+  manejarClickElemento();
+}
+
+function manejarClickElemento() {
+  const elementos = document.querySelectorAll(
+    ".lista_elemento_nombre_contenedor"
+  );
+
+  elementos.forEach((elemento) => {
+    let clickTimer = null;
+
+    elemento.addEventListener("click", (e) => {
+      if (clickTimer !== null) return;
+
+      const target = e.currentTarget;
+      const contenedor = target.closest(".lista_elemento");
+      const detalle = contenedor.querySelector(
+        ".lista_elemento_datos_secundarios"
+      );
+
+      clickTimer = setTimeout(() => {
+        detalle.classList.toggle("activo");
+        console.log("Click simple en:", target);
+        clickTimer = null;
+      }, 250);
+    });
+
+    elemento.addEventListener("dblclick", (e) => {
+      clearTimeout(clickTimer);
+      clickTimer = null;
+      console.log("Doble click en:", e.currentTarget);
+    });
+  });
 }
 
 function animar_lista() {
